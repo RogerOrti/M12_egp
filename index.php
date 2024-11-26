946e9b2896082e9f07f6b8e105c2de20df34116b
RogerOrti <rortim2223@politecnics.barcelona>
2024-11-26 19:10:13 +0100

    Add login and register php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <div class="bg-color-warning">
 
   </div>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -36,9 +36,14 @@
           </ul>
         </li>
       </ul>
+      <div class="d-flex">
+        <a href="login.php" class="btn btn-outline-primary">Iniciar sessió</a>
+        <a href="register.php" class="btn btn-primary">Registrar-se</a>
+      </div>
     </div>
   </div>
 </nav>
+
 </body>
 <script src="bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
 </html>
