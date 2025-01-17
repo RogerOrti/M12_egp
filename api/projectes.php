6980cb723b3de7eff1ea1d50060829a25839d303
RogerOrti <rortim2223@politecnics.barcelona>
2025-01-17 19:24:13 +0100

    Trim whitespces

--- api/projectes.php
+++ api/projectes.php
@@ -12,13 +12,11 @@ FROM projectes p
 INNER JOIN usuaris_projecte_rol upr ON p.id = upr.id_projectes
 WHERE upr.id_usuaris = :id_usuari";
 
-    $sentencia = $conn->prepare($sentencia_text);
-    $sentencia->bindParam(':id_usuari', $id_usuari, PDO::PARAM_INT);
-    $sentencia->execute();
-    $sentencia->setFetchMode(PDO::FETCH_ASSOC);
-    $resultat = $sentencia->fetchAll();
-
-    echo json_encode($resultat);
-
+$sentencia = $conn->prepare($sentencia_text);
+$sentencia->bindParam(':id_usuari', $id_usuari, PDO::PARAM_INT);
+$sentencia->execute();
+$sentencia->setFetchMode(PDO::FETCH_ASSOC);
+$resultat = $sentencia->fetchAll();
+echo json_encode($resultat);
 
 $conn = null;
