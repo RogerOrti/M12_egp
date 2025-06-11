5945abb06cf93db30cee495a5e06b723a678db1d
RogerOrti <rortim2223@politecnics.barcelona>
2025-06-11 17:11:18 +0200

    Prova de workflow

--- login.php
+++ login.php
@@ -12,6 +12,7 @@
     ?>
     <div class="container mt-5">
         <h2 class="text-center">Inici sessió</h2>
+        <p>Prova de deploy</p>   
         
     <?php 
         include_once('php_partials/missatges.php');  
